d1d483431df6632d8c75b8180cd7536d425c21d1
smt923 <smtea923@gmail.com>
2017-07-25 01:48:11 +0100

    Add gif trimming, updated readme

--- README.md
+++ README.md
@@ -6,16 +6,24 @@ View details about a gif and split it into individual frames
 Running with just an input gif will simply print some information, run with `-s` to split the gif into individual frames, which by default save to `./out`
 
 Basic usage, to simply print some information about the gif:
-```
+```bash
 ./gifspector input.gif
 ```
 
-Splitting the file (-s), changing the default output folder (-o) and saving each frame as jpegs (-j)
+You can also trim a gif by specifying the start and, optionally, end frames to trim to (exclusive):
+```bash
+./gifspector input.gif 10 # trim from frame 10 to the last frame
 ```
+```bash
+./gifspector input.gif 8 10 # trim from frame 8 to frame 10 (2 frames)
+```
+
+Splitting the file (-s), changing the default output folder (-o) and saving each frame as jpegs (-j)
+```bash
 ./gifspector input.gif -s -o frames -j
 ```
 
-These are the stats it prints out for a gif
+These are the stats printed out for a gif file:
 ```
 --- GIF STATS: in.gif ---
 Number of frames:
--- gifspector.go
+++ gifspector.go
@@ -14,22 +14,25 @@ func main() {
 	savefolder := kingpin.Flag("output", "Output folder name").Default("out").Short('o').String()
 	shouldsplit := kingpin.Flag("split", "Split the input gif into individual frames?").Short('s').Bool()
 	savejpeg := kingpin.Flag("jpeg", "Save individual frames as JPEGs instead of GIFs").Short('j').Bool()
+	//shouldtrim := kingpin.Flag("trim", "Trim the gif (saved as 'trimmed.gif') using a start and end frame number (Passed as arguments, see --help)").Short('t').Bool()
+	starttrim := kingpin.Arg("start", "Start frame to trim the gif from (Inclusive)").Default("-1").Int()
+	endtrim := kingpin.Arg("end", "The frame to trim the gif up to (Exclusive)").Default("-1").Int()
 	kingpin.CommandLine.HelpFlag.Short('h')
 	kingpin.Parse()
 
 	f, err := os.Open(*gifname)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		fmt.Fprintf(os.Stderr, "Could not open image \"%s\"! Exiting.", *gifname)
+		fmt.Fprintf(os.Stderr, "Could not open image \"%s\"! Exiting.\n", *gifname)
 		os.Exit(1)
 	}
 	defer f.Close()
 
 	// Decode our single gif into a struct containing every frame & some other useful information
-	split, err := gif.DecodeAll(f)
+	gifstruct, err := gif.DecodeAll(f)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		fmt.Fprintf(os.Stderr, "Unable to decode \"%s\" as a GIF! Exiting.", *gifname)
+		fmt.Fprintf(os.Stderr, "Unable to decode \"%s\" as a GIF! Exiting.\n", *gifname)
 		os.Exit(1)
 	}
 
@@ -38,23 +41,30 @@ func main() {
 		os.Mkdir(*savefolder, 0644)
 	}
 
-	printStats(split, *savefolder, *gifname, *shouldsplit)
+	printStats(gifstruct, *savefolder, *gifname, *shouldsplit)
 
 	if *shouldsplit {
-		SplitGIF(split, *savefolder, *savejpeg)
+		SplitGIF(gifstruct, *savefolder, *savejpeg)
 	}
-}
 
-// Prints out a range of interesting stats about the input gif
-func printStats(gifstruct *gif.GIF, path string, name string, shouldsplit bool) {
-	fmt.Printf("--- GIF STATS: %s ---\n", name)
-	fmt.Printf("Number of frames:\n %d\n", len(gifstruct.Image))
-	fmt.Printf("Delay per frame (100ths / sec):\n %d\n", gifstruct.Delay)
-	fmt.Printf("Loop count:\n %d\n", gifstruct.LoopCount)
-	fmt.Printf("Image size (height x width):\n %d x %d\n", gifstruct.Config.Height, gifstruct.Config.Width)
-	if shouldsplit {
-		fmt.Printf("\nOutputting individual frames to folder: %s\n", path)
+	if *starttrim != -1 {
+		start, end := normalizeTrimLength(*starttrim, *endtrim, gifstruct)
+		ftrim, err := os.Create("trimmed.gif")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintf(os.Stderr, "Unable to open trimmed.gif for writing! Exiting.\n")
+			os.Exit(1)
+		}
+		defer ftrim.Close()
+		newgif := TrimGIF(start, end, gifstruct)
+		err = gif.EncodeAll(ftrim, newgif)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintf(os.Stderr, "Unable to encode trimmed.gif! Exiting.\n")
+			os.Exit(1)
+		}
 	}
+
 }
 
 // SplitGIF will split a gif (gifstruct) into it's individual frames then save them into a folder (savefolder), optionally as JPEGs (saveasjpeg)
@@ -79,4 +89,42 @@ func SplitGIF(gifstruct *gif.GIF, savefolder string, saveasjpeg bool) {
 			}
 		}
 	}
+}
+
+// TrimGIF will take a starting gif and trim it down to a start and end frame from the original, then return the new gif
+func TrimGIF(start int, end int, original *gif.GIF) *gif.GIF {
+	newgif := original
+	newgif.Image = original.Image[start:end]
+	newgif.Delay = original.Delay[start:end]
+	newgif.Disposal = original.Disposal[start:end]
+	return newgif
+}
+
+// Prints out a range of interesting stats about the input gif
+func printStats(gifstruct *gif.GIF, path string, name string, shouldsplit bool) {
+	fmt.Printf("--- GIF STATS: %s ---\n", name)
+	fmt.Printf("Number of frames:\n %d\n", len(gifstruct.Image))
+	fmt.Printf("Delay per frame (100ths / sec):\n %d\n", gifstruct.Delay)
+	fmt.Printf("Loop count:\n %d\n", gifstruct.LoopCount)
+	fmt.Printf("Image size (height x width):\n %d x %d\n", gifstruct.Config.Height, gifstruct.Config.Width)
+	if shouldsplit {
+		fmt.Printf("\nOutputting individual frames to folder: %s\n", path)
+	}
+}
+
+func normalizeTrimLength(startl, endl int, gifstruct *gif.GIF) (start, end int) {
+	start = startl
+	end = endl
+
+	if start == -1 {
+		start = 0
+	}
+	if end == -1 {
+		end = len(gifstruct.Image)
+	}
+	if start > end {
+		fmt.Fprintf(os.Stderr, "Invalid trim length! The end frame must be larger than the start. Exiting.\n")
+		os.Exit(1)
+	}
+	return start, end
 }
